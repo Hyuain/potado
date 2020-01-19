e0de2426c5e63a6af8e37a7bf6081c8dc91d18db
Hyuain <Hyuain97@163.com>
2020-01-19 18:22:57 +0800

    增加主页的注销，和未登录时的跳转

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -28,6 +28,9 @@ instance.interceptors.response.use((response) => {
   }
   return response;
 }, (error) => {
+  if(error.response.status === 401){
+    window.location.href = '/login'
+  }
   return Promise.reject(error);
 });
 
