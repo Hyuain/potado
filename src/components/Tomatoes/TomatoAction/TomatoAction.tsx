80efe5c8a2d65be72d6e275094a212f65614c641
Hyuain <Hyuain97@163.com>
2020-01-24 12:06:32 +0800

    组件：TomatoAction 内容：增加三个状态，没有未完成（显示按钮）、正在倒计时、倒计时结束（弹出 Input，添加 description） 问题：- 解决：-

--- src/components/Tomatoes/TomatoAction/TomatoAction.tsx
+++ src/components/Tomatoes/TomatoAction/TomatoAction.tsx
@@ -1,38 +1,65 @@
 import React from 'react';
 import axios from '../../../config/axios';
 
-import {connect} from 'react-redux';
-import actions from '../../../redux/actions/index'
-
-import {Button} from 'antd';
+import {Button, Input} from 'antd';
 import './TomatoAction.less';
 
-const TomatoAction = () => {
-  const startTomato = async () => {
+interface ITomatoActionProps {
+  startTomato: () => void,
+  unfinishedTomato: any
+}
+
+const TomatoAction = (props: ITomatoActionProps) => {
+
+  const [description, setDescription] = React.useState<string>('');
+
+  let html = <div/>;
+
+  const onKeyup = (e: any) => {
+    if (e.keyCode === 13 && description !== '') {
+      addDescription();
+    }
+  };
+
+  const addDescription = async () => {
     try {
-      const response = await axios.post('tomatoes', {duration: 25 * 60 * 1000});
-      console.log(response.data);
+      const response = await axios.put(`tomatoes/${props.unfinishedTomato.id}`, {description});
+      setDescription('');
+      console.log(response);
     } catch (e) {
 
     }
   };
 
+  if (props.unfinishedTomato === undefined) {
+    html = <Button className="start-button" onClick={props.startTomato}>开始番茄</Button>;
+  } else {
+    const startAt = Date.parse(props.unfinishedTomato.start_at);
+    const duration = props.unfinishedTomato.duration;
+    const timeNow = new Date().getTime();
+    if (timeNow - startAt > duration) {
+      html = (
+        <div>
+          <Input
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            onKeyUp={onKeyup}
+          />
+        </div>
+      );
+    } else {
+      html = <div>倒计时</div>;
+    }
+  }
+
+
   return (
     <div className="tomato-action">
-      <Button className="start-button" onClick={startTomato}>开始番茄</Button>
+      {html}
     </div>
   );
 };
 
-const mapStateToProps = (state:any, ownProps: any) => ({
-  ...ownProps
-});
-
-const mapDispatchToProps = {
-  addTomato: actions.addTomato
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TomatoAction);
+export default TomatoAction;
