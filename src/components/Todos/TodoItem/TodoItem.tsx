4a7cba8ec012a0c1bb55e9f655cda2c433e46169
Hyuain <Hyuain97@163.com>
2020-02-07 21:40:06 +0800

    组件：Todos 内容：优化样式 问题：- 解决：-

--- src/components/Todos/TodoItem/TodoItem.tsx
+++ src/components/Todos/TodoItem/TodoItem.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import {connect} from 'react-redux';
 import actions from '../../../redux/actions';
 
-import {Checkbox, Icon} from 'antd';
+import {Checkbox, Icon, message} from 'antd';
 import './TodoItem.less';
 
 interface ITodoItemProps {
@@ -21,25 +21,46 @@ const TodoItem = (props: ITodoItemProps) => {
 
   const [textContent, setTextContent] = React.useState(props.description);
 
+  const onClickEnter = () => {
+    if (inputCheck()) {
+      updateTodo({description: textContent});
+    }
+  };
+
+  const onClickDelete = () => {
+    updateTodo({deleted: true});
+  };
+
+  const onKeyUp = (e: any) => {
+    if (e.keyCode === 13) {
+      if (inputCheck()) {
+        updateTodo({description: textContent});
+      }
+    }
+  };
+
+  const inputCheck = () => {
+    if (textContent === '') {
+      message.warning('还是说点儿什么吧');
+      return false;
+    }
+    return true;
+  };
+
   const updateTodo = async (params: any) => {
-    if(params.completed){
-      params.completed_at = new Date()
+    if (params.completed) {
+      params.completed_at = new Date();
     }
     try {
       const response = await axios.put(`todos/${props.id}`, params);
       props.updateTodo(response.data.resource);
     } catch (e) {
+      message.error('网络好像有点不太好哦，一会儿再试吧');
     }
   };
 
   const editTodo = () => {
     props.editTodo(props.id);
-  };
-
-  const onKeyup = (e: any) => {
-    if (e.keyCode === 13 && textContent !== '') {
-      updateTodo({description: textContent});
-    }
   };
 
   const EditField = (
@@ -49,13 +70,11 @@ const TodoItem = (props: ITodoItemProps) => {
              onChange={(e) => {
                setTextContent(e.target.value);
              }}
-             onKeyUp={onKeyup}
+             onKeyUp={onKeyUp}
       />
       <div className="icon-wrapper">
-        <Icon type="enter" onClick={() => {
-          updateTodo({description: textContent});
-        }}/>
-        <Icon type="delete" theme="filled" onClick={() => updateTodo({deleted: true})}/>
+        <Icon type="enter" onClick={onClickEnter}/>
+        <Icon type="delete" theme="filled" onClick={onClickDelete}/>
       </div>
     </div>
   );
