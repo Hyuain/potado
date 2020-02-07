7872b1d3428cc1de4bceb0ad9a697863f556bbca
Hyuain <Hyuain97@163.com>
2020-02-07 11:33:51 +0800

    Updates

---
+++ precache-manifest.26187d6ab35b11846b4d5ab1cbcd0989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64d794430c9326cb4ea76d07c9fc69d",
+    "url": "/potado/index.html"
+  },
+  {
+    "revision": "8f1e25e1fff1af228dbc",
+    "url": "/potado/static/css/2.86b79581.chunk.css"
+  },
+  {
+    "revision": "f5e50468f8cf80ad7eae",
+    "url": "/potado/static/css/main.744a6d52.chunk.css"
+  },
+  {
+    "revision": "8f1e25e1fff1af228dbc",
+    "url": "/potado/static/js/2.2f4065d7.chunk.js"
+  },
+  {
+    "revision": "fb08aec389ff1d0e3b63dd26ad5ac153",
+    "url": "/potado/static/js/2.2f4065d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5e50468f8cf80ad7eae",
+    "url": "/potado/static/js/main.1850d7df.chunk.js"
+  },
+  {
+    "revision": "cf39d969bd37e04070d3",
+    "url": "/potado/static/js/runtime-main.934bb581.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/patado/precache-manifest.a8f20cfc3c0f6d6d02e537f5aafccc83.js"
+  "/potado/precache-manifest.26187d6ab35b11846b4d5ab1cbcd0989.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/patado/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/potado/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
